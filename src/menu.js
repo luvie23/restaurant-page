a116522a73957c1604426979b4717fa1ae32ff1b
Luvie Rallos <luvierallos@gmail.com>
2022-10-19 12:18:25 +0800

    finish menu items and adjust view for smaller screens

--- src/menu.js
+++ src/menu.js
@@ -26,18 +26,23 @@ export default function menu(){
     item4.setAttribute('id', 'item4');
     item4.classList.add('items');
 
+    const paragraph = document.createElement('p');
+    paragraph.textContent = "GOOD FOOD, BETTER COMPANY"
+
     //food description
     item1.appendChild(createDiv()).textContent = "Breakfast Set (Sausage, Rice, Eggs)";
     item2.appendChild(createDiv()).textContent = "SALAD INSIDE AN AVOCADO";
-    item3.appendChild(createDiv());
-    item4.appendChild(createDiv());
+    item3.appendChild(createDiv()).textContent = "SLOPPY BURGER";
+    item4.appendChild(createDiv()).textContent = "SEAFOOD RAMEN";
     
+    content.appendChild(paragraph);
     content.appendChild(board);
-
+    
     board.appendChild(item1);
     board.appendChild(item2);
     board.appendChild(item3);
     board.appendChild(item4);
+    
 
     
    
